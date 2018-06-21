7584b54d5f9baea2a25498b29f8cf648cc5d630a
Zxongfu <sonis123000@gmail.com>
2018-06-21 14:30:54 +0800

    change print

--- javapractice/src/main/java/com/example/javapractice/MyClass.java
+++ javapractice/src/main/java/com/example/javapractice/MyClass.java
@@ -3,5 +3,7 @@ package com.example.javapractice;
 public class MyClass {
     public static void main(String[] args) {
         System.out.print(10);
+        System.out.print(10);
+        System.out.print(10);
     }
 }
